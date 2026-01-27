71b90c845480e2a7535784226b60ff4f80812ada
Mark <mark@markyourdata.com>
2026-01-27 08:35:43 +0100

    add hotjar

--- js/analytics.js
+++ js/analytics.js
@@ -1,18 +1,28 @@
 // Google Analytics 4 Setup
 // Measurement ID: G-H10MC1RNXH
 
-(function() {
+(function () {
   // Load gtag.js asynchronously
-  var script = document.createElement('script');
+  var script = document.createElement("script");
   script.async = true;
-  script.src = 'https://www.googletagmanager.com/gtag/js?id=G-H10MC1RNXH';
+  script.src = "https://www.googletagmanager.com/gtag/js?id=G-H10MC1RNXH";
   document.head.appendChild(script);
 
   // Initialize dataLayer and gtag function
   window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
+  function gtag() {
+    dataLayer.push(arguments);
+  }
   window.gtag = gtag;
 
-  gtag('js', new Date());
-  gtag('config', 'G-H10MC1RNXH');
+  gtag("js", new Date());
+  gtag("config", "G-H10MC1RNXH");
+})();
+
+// Contentsquare (Hotjar) Setup
+(function () {
+  var script = document.createElement("script");
+  script.async = true;
+  script.src = "https://t.contentsquare.net/uxa/fbea3917abc9c.js";
+  document.head.appendChild(script);
 })();
